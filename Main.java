aea959a3dd8a275b8def4f8b0460a6aebfef5f76
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-05-22 12:34:28 -0400

    finished code

--- Main.java
+++ Main.java
@@ -1,9 +1,30 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.lang.String;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
+        /* write a program which prompts the user to ip their name
+        the program should greet names Alice and Bob */
+        //use control flow, cond, user ip, obj inst/dec, method invok
+        
+        /* from IO lecture notes */
+        System.out.println("What is your name? ");
+        
+        Scanner in = new Scanner(System.in);
+        String firstName = in.next();
 
+        if (firstName.equals("Alice") || firstName.equals("Bob"))
+        {
+            System.out.println("Nice to meet you!");
+        } else {
+            return;
+        }
+
+        
     }
 }
